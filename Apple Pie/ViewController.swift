8cde449f9c3e23f02f51704cfe444ad15718d35f
J Palacio <jaypalacio95@gmail.com>
2024-07-10 23:27:11 -0400

    finished setting up UI

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
